5cae5a0bcc869c9a95a40dd91b1d877373ecba07
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-05-29 00:13:03 +0100

    create header container  and styling

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,22 @@
 import React from 'react';
 import './Header.css';
+
+import HeaderTitle from './HeaderTitle';
+import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
+
+function HeaderContainer () {
+    return (
+        <div className='HeaderContainer'>
+            <ImageThumbnail imgUrl = "https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png"/>
+            <div className='header-content'>
+            <HeaderTitle headerText = " Lambda School  " handle=" @LambdaSchool "
+             timestamp=" &middot; 28 May " /> 
+            <HeaderContent content = "Let's learn React by building simple interfaces with components. Don't try to 
+            overthink it, just keep it simple and have fun.Once you feel comfortable using components you are  well on your way to learn React!" />  
+            </div>        
+        </div>
+   );
+}
+
+export default  HeaderContainer ;
