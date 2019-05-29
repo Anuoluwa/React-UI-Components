be329e594dbedaad9df35a80bce3d831270bbd8e
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-05-29 01:07:19 +0100

    completes mvp

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+
+function CardContainer () {
+    return (
+        <div className='CardContainer'>
+            <CardBanner imgUrl = "https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"/>
+            <div className='card-content'>
+            <CardContent header="Get Started with React" body="React makes it painless to create interactive UIs. Design simple views for each state in your application." footer="reactjs.org" /> 
+            </div>        
+        </div>
+   );
+}
+
+export default  CardContainer ;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Card.css';
+
+function CardContent ({header, body, footer}) {
+    return (
+        <div className="CardContent">
+            <h5>{header}</h5>
+            <p>{body}</p>
+            <span>{footer}</span>
+        </div>
+    );
+}
+
+export default CardContent;
