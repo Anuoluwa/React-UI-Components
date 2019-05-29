862b627d83f9da6aa6903a33b66ac2f54953bae4
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-05-29 01:20:54 +0100

    completes mvp- add anchor tag to card container

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -6,12 +6,15 @@ import CardContent from './CardContent';
 
 function CardContainer () {
     return (
-        <div className='CardContainer'>
-            <CardBanner imgUrl = "https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"/>
-            <div className='card-content'>
-            <CardContent header="Get Started with React" body="React makes it painless to create interactive UIs. Design simple views for each state in your application." footer="reactjs.org" /> 
-            </div>        
-        </div>
+        <a href="https://www.reactjs.org">
+            <div className='CardContainer'>
+                <CardBanner imgUrl = "https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png"/>
+                <div className='card-content'>
+                <CardContent header="Get Started with React" body="React makes it painless to create interactive UIs. Design simple views for each state in your application." footer="reactjs.org" /> 
+                </div>        
+            </div>  
+
+        </a>
    );
 }
 
